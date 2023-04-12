ded73371ac2564595ab408243c3d00788b9dceaf
Minsu Lee <amond@amond.net>
2023-04-12 20:01:25 +0900

    refactor: analytics store

---
+++ flutter_news_example/lib/analytics/store/analytics_store.dart
@@ -0,0 +1,12 @@
+import 'package:mobx/mobx.dart';
+
+// name: AnalyticsStore
+// Include generated file
+part 'analytics_store.g.dart';
+
+// This is the class used by rest of your codebase
+class AnalyticsStore = AnalyticsStoreBase with _$AnalyticsStore;
+
+// The store-class
+abstract class AnalyticsStoreBase with Store {
+}
